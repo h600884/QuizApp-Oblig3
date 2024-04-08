2552422e4248f567221b5df197f75fcadad822cf
h600884 <600884@stud.hvl.no>
2024-04-08 14:14:19 +0200

    Database implementert, mangler funksjonalitet

--- app/src/main/java/no/hvl/dat153/quizapp_oblig3/ImageViewModel.java
+++ app/src/main/java/no/hvl/dat153/quizapp_oblig3/ImageViewModel.java
@@ -1,5 +1,43 @@
 package no.hvl.dat153.quizapp_oblig3;
 
-public class ImageViewModel {
-    
+import android.app.Application;
+import android.media.Image;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class ImageViewModel extends AndroidViewModel {
+
+    private ImageRepository imageRepository;
+
+    private LiveData<List<ImageEntity>> images;
+
+    public ImageViewModel(Application application) {
+        super(application);
+        imageRepository = new ImageRepository(application);
+        images = imageRepository.getAllImages();
+    }
+
+    LiveData<List<ImageEntity>> getAllImages() {
+        return images;
+    }
+
+    public void insert(ImageEntity image) {
+        imageRepository.insert(image);
+    }
+
+    public void deleteWithId(long id) {
+        imageRepository.deleteWithId(id);
+    }
+
+    public LiveData<List<ImageEntity>> getAllImagesDesc() {
+        return imageRepository.getAllImageDesc();
+    }
+
+    public LiveData<List<ImageEntity>> getAllImagesAsc() {
+        return imageRepository.getAllImageAsc();
+    }
+
 }
